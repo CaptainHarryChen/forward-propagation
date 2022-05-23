2d198e041a06ba86d3a3f996a265d1f0b5b60085
Captain Chen <2429643516@qq.com>
2022-05-23 20:35:44 +0800

    添加前向传播的线性层类

---
+++ ForwardPropagation/Layer.py
@@ -0,0 +1,18 @@
+import numpy as np
+import ForwardModel
+from ForwardTensor import FTensor
+
+
+class Linear():
+    def __init__(self, in_features, out_features, model, name):
+        self.w = FTensor(np.random.standard_normal(
+            [out_features, in_features]))
+        model.register_param(self.w, name+"_w")
+        self.b = FTensor(np.random.standard_normal([out_features, 1]))
+        model.register_param(self.b, name+"_b")
+
+    def __call__(self, x):
+        x = FTensor.expand_dims(x, axis=-1)
+        a = FTensor.matmul(self.w, x)+self.b
+        a = FTensor.squeeze(a, axis=-1)
+        return a
