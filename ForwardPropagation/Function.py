7af66518209e23b19511b90fd9a77d675be8a995
Captain Chen <2429643516@qq.com>
2022-05-23 23:04:31 +0800

    优化relu

--- ForwardPropagation/Function.py
+++ ForwardPropagation/Function.py
@@ -2,34 +2,33 @@ import numpy as np
 from ForwardTensor import FTensor
 
 
-def abs(a):
-    ret=FTensor(a)
-    ret.value[a.value<0]=-a.value[a.value<0]
-    ret.delta[a.value<0]=-a.delta[a.value<0]
+def relu(a):
+    ret = FTensor((np.abs(a.value)+a.value)/2, a.delta)
+    ret.delta = ret.delta*np.sign(ret.value)
     return ret
 
-def relu(a):
-    ret=FTensor(a)
-    ret.value[a.value<0]=0
-    ret.delta[a.value<0]=0
-    return ret
 
 def log(a):
-    return FTensor(np.log(a.value),a.delta/a.value)
+    return FTensor(np.log(a.value), a.delta/a.value)
+
 
 def exp(a):
-    return FTensor(np.exp(a.value),np.exp(a.value)*a.delta)
+    return FTensor(np.exp(a.value), np.exp(a.value)*a.delta)
+
 
 def sigmoid(a):
     return 1/(1+exp(-a))
 
-def cross_entropy(a,y):
+
+def cross_entropy(a, y):
     return -(log(a)*y+log(1-a)*(1-y))
 
+
 def average(a):
-    return FTensor(np.average(a.value),np.average(a.delta))
+    return FTensor(np.average(a.value), np.average(a.delta))
+
 
 def softmax(x, axis=-1):
-	e_x = exp(x)
-	probs = e_x / np.sum(e_x.value, axis=axis, keepdims=True)
-	return probs
+    e_x = exp(x)
+    probs = e_x / np.sum(e_x.value, axis=axis, keepdims=True)
+    return probs
