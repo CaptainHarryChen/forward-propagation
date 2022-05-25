5016ca1e0537a0d177d7075a4788365ec4e9beaa
Captain Chen <2429643516@qq.com>
2022-05-25 21:22:28 +0800

    测试多线程

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import torch
 import time
-import numpy as np
+from concurrent.futures import ThreadPoolExecutor
 
-X = torch.tensor(np.random.standard_normal([128,128]),dtype=torch.float32)
-Y = torch.tensor(np.random.standard_normal([128,128]),dtype=torch.float32)
-tt1 = time.time()
-Z = torch.nn.functional.linear(X, Y)
-print(time.time()-tt1)
+def fun(x):
+    print(f"hahah{x}")
+    return x+1
+
+with ThreadPoolExecutor(max_workers=2) as executor:
+    ans = executor.map(fun, [1,2,3,4])
+    for res in ans:
+        print(res)
